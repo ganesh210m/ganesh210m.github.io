c4abd74997694d68dd0dd6343b144fcd4f23b6b5
srvinith <r36041595@gmail.com>
2023-07-19 13:11:38 +0530

    react add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h2>Hi This Ganesh</h2>
+   <h4>Ui & Ux Designer</h4>
+    </div>
+  );
+}
+
+export default App;
